8f24181732d59e2a3030119068e7075f01a45846
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-06-09 23:08:20 +0530

    Socket setup

---
+++ server/src/socket/socket.ts
@@ -0,0 +1,23 @@
+import { Server } from "socket.io";
+import { Server as HttpServer } from "http";
+
+let io: Server;
+
+export const initSocket = (server: HttpServer) => {
+  io = new Server(server, {
+    cors: {
+      origin: process.env.CLIENT,
+      credentials: true,
+    },
+  });
+
+  return io;
+};
+
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+
+  return io;
+};
