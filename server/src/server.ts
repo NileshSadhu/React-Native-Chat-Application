e67bdf9563e6d176f3d54e140af3403e7bcf1193
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-06-09 23:10:13 +0530

    Update server/ts file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,46 @@
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import app from "./app.js";
+import connectDB from "./config/db";
+import app from "./app";
+import { createServer } from "http";
+import { initSocket } from "./socket/socket";
+import { socketAuth } from "./middleware/socket.middleware";
+import { Message } from "./models/message.model";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 9000;
 
+const httpServer = createServer(app);
+
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is listening at http://localhost:${PORT}`);
+    const io = initSocket(httpServer);
+
+    io.use(socketAuth);
+
+    io.on("connection", (socket) => {
+      console.log("User connected:", socket.data.user.username);
+
+      socket.on("send-message", async (data) => {
+        const user = socket.data.user;
+        const message = await Message.create({
+          senderId: user.userId,
+          senderName: user.username,
+          text: data.text,
+        });
+
+        io.emit("receive-message", message);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+      });
+    });
+
+    httpServer.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
     });
   })
   .catch((error) => {
-    console.log("Error while connecting.");
     console.log(error);
   });
